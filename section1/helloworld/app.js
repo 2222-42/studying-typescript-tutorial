1d119abfb32db44626e485aab1d420bb9cb8f1ba
2222-42 <stephen0617@me.com>
2021-03-28 14:16:58 +0900

    chore(sect.1): *

--- section1/helloworld/app.js
+++ section1/helloworld/app.js
@@ -1,4 +1,9 @@
 var message = 'Hello, World!';
-let heading = document.createElement('h1');
+var heading = document.createElement('h1');
 heading.textContent = message;
-document.body.appendChild(heading)
+document.body.appendChild(heading);
+console.log('Hello'.length);
+console.log('Hello'.toLocaleUpperCase());
+var heading2 = document.querySelector('h1');
+// console.log(heading2.getAttribute);
+console.log(heading2 === null || heading2 === void 0 ? void 0 : heading2.getAttribute);
--- section1/helloworld/app.ts
+++ section1/helloworld/app.ts
@@ -2,3 +2,9 @@ let message: string = 'Hello, World!';
 let heading = document.createElement('h1');
 heading.textContent = message;
 document.body.appendChild(heading)
+
+console.log('Hello'.length);
+console.log('Hello'.toLocaleUpperCase()); 
+const heading2 = document.querySelector('h1');
+// console.log(heading2.getAttribute);
+console.log(heading2?.getAttribute);
