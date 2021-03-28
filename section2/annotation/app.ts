d5ce7ed518cb17a90730990b69725c3955a6a9ac
2222-42 <stephen0617@me.com>
2021-03-29 08:09:01 +0900

    chore(sect.2): type annotation

---
+++ section2/annotation/app.ts
@@ -0,0 +1,17 @@
+let counter: number;
+counter = 1;
+// counter = 'Hello';
+let counter2: number = 1;
+let _name: string = 'John';
+let age: number = 25;
+let active: boolean = true;
+
+let names: string[] = ['John', 'Jane', 'Peter', 'David', 'Mary'];
+let person: {
+    name: string;
+    age: number
+};
+person = {
+    name: _name,
+    age: age,
+}
