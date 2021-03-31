9edcc80b5e00040704167759895ecf04ba3e42ca
2222-42 <stephen0617@me.com>
2021-03-31 10:24:34 +0900

    feat(sect.2): TypeScript String

---
+++ section2/string/app.ts
@@ -0,0 +1,11 @@
+export { };
+
+let firstNmae: string = 'John';
+let title: string = "Web Developer";
+let profile: string = `I'm ${firstNmae}.
+I'm a ${title}`;
+console.log(profile)
+let description = `This TypeScript string can 
+span multiple 
+lines
+`;
