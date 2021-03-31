2a91604dc126c619ddd035bfb5d1ea21425f9849
2222-42 <stephen0617@me.com>
2021-03-31 10:27:05 +0900

    chore(sect.2): TypeScript Boolean

---
+++ section2/bool/app.ts
@@ -0,0 +1,10 @@
+export { };
+
+let pending: boolean;
+pending = true;
+console.log(pending);
+// JavaScript has the Boolean type that refers to the non-primitive boxed object. 
+// The Boolean type has the letter B in uppercase, which is different from the boolean type.
+
+pending = false;
+console.log(pending);
