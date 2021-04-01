b2b6a3244f4cd24e1e8775033b05cbef6502f927
2222-42 <stephen0617@me.com>
2021-04-02 08:49:00 +0900

    feat(sect.2): TypeScript Tuple

---
+++ section2/tuple/app.ts
@@ -0,0 +1,11 @@
+export { };
+
+let skill: [string, number];
+skill = ['Programming', 5];
+// skill = [5, 'Programming'];
+
+let color: [number, number, number] = [255, 0, 0];
+
+let bgColor, headerColor: [number, number, number, number?];
+bgColor = [0, 255, 255, 0.5];
+headerColor = [0, 255, 255];
