180787aa7923d14e32b2d46739857d5a1d58951e
2222-42 <stephen0617@me.com>
2021-04-04 17:41:02 +0900

    feat(sect.2): typescript any type

---
+++ section2/any/app.ts
@@ -0,0 +1,14 @@
+const json = `{"latitude": 10.11, "longitude":12.12}`;
+
+const currentLocation = JSON.parse(json);
+console.log(currentLocation);
+console.log(currentLocation.x);
+
+let result;
+result = 10.123;
+console.log(result.toFixed());
+// result.willExist();
+
+// let result2: object;
+// result2 = 10.123;
+// result2.toFixed();
