c2b447ad2c927f86ea9d6e5e8ac51768f2bc65c2
2222-42 <stephen0617@me.com>
2021-04-07 08:53:17 +0900

    feat(sect.3): typescript-do-while

---
+++ section3/do_while/app.ts
@@ -0,0 +1,22 @@
+export { };
+
+console.log("i");
+let i = 0;
+
+do {
+    console.log(i);
+    i++;
+} while (i < 10);
+
+console.log("j");
+let j = 100;
+
+do {
+    console.log(j);
+    j++;
+} while (j < 10);
+
+while (j < 100) {
+    console.log(j);
+    j++;
+}
