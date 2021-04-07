8194feaa46eacfd360e5d7edbe4e88d458990ad3
2222-42 <stephen0617@me.com>
2021-04-07 09:06:28 +0900

    feat(sect.3): typescript-continue

---
+++ section3/continue/app.ts
@@ -0,0 +1,34 @@
+export { };
+
+// The `continue` statement skips to the end of the loop and continues the next iteration.
+for (let index = 0; index < 9; index++) {
+    if (index % 2)
+        continue;
+
+    console.log(index);
+
+}
+
+let index = -1;
+
+while (index < 9) {
+    index = index + 1;
+    if (index % 2)
+        continue;
+
+    console.log(index);
+}
+
+index = 9;
+let count = 0;
+
+do {
+    index += 1;
+    if (index % 2)
+        continue;
+
+    count += 1;
+
+} while (index < 99);
+
+console.log(count);
