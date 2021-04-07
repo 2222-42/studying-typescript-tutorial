61b24a45fb6e68dabc2eb96271a665dd01554e18
2222-42 <stephen0617@me.com>
2021-04-07 09:00:59 +0900

    feat(sect.3): typescript-break

---
+++ section3/break/app.ts
@@ -0,0 +1,37 @@
+export { };
+
+// Use `break` statement to terminate a loop or switch.
+
+let products = [
+    { name: 'phone', price: 700 },
+    { name: 'tablet', price: 900 },
+    { name: 'laptop', price: 1200 },
+]
+
+// let iを使うとforの外ではiは生きない。
+for (var i = 0; i < products.length; i++) {
+    if (products[i].price == 900)
+        break;
+
+}
+
+console.log(products[i]);
+
+let discount = 0;
+let product = products[1];
+
+switch (product.name) {
+    case 'phone':
+        discount = 5;
+        break;
+    case 'tablet':
+        discount = 10;
+        break;
+    case 'laptop':
+        discount = 15;
+        break;
+    default:
+        break;
+}
+
+console.log(`There is a ${discount}% on ${product.name}`);
