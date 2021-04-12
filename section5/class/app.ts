6a86d594c390ac3c142fff6815639900c23a5e89
2222-42 <stephen0617@me.com>
2021-04-13 06:37:39 +0900

    feat(sect.5): typescript-class

---
+++ section5/class/app.ts
@@ -0,0 +1,19 @@
+export { };
+
+class Person {
+    ssn;
+    firstName;
+    lastName;
+    constructor(ssn: string, firstName: string, lastName: string) {
+        this.ssn = ssn;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+let person = new Person('171-28-0926', 'John', 'Doe');
+console.log(person.getFullName());
